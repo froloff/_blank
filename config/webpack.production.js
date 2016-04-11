6c6bfbec7adee0f1e2ab8b306142e52ae6b68f4c
Leonid Frolov <leonid@froloff.me>
2016-04-11 13:24:27 +0300

    Fix for vendors.js bundle

--- config/webpack.production.js
+++ config/webpack.production.js
@@ -10,7 +10,7 @@ module.exports = {
     app: [
       path.resolve(__dirname, '../client/index.jsx'),
     ],
-    vendors: ['react', 'react-dom', 'react-redux', 'redux'],
+    vendors: ['react', 'react-dom', 'react-redux', 'redux', 'redux-thunk', 'redux-promise'],
   },
   output: {
     path: path.resolve(__dirname, '../public'),
