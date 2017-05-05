7dbeb4512ea183f8f2ef14661451679720fd4cea
Leonid Frolov <leonid@froloff.me>
2017-05-05 18:51:02 +0300

    Make synchronous action asynchronous

--- client/actions/index.js
+++ client/actions/index.js
@@ -3,15 +3,19 @@
 import * as types from 'constants/Actions';
 
 // Asynchronous Action
-export function increment() {
-  return {
-    type: types.COUNTER_INCREMENT,
+export function decrement() {
+  return (dispatch) => {
+    setTimeout(() => {
+      dispatch({
+        type: types.COUNTER_DECREMENT,
+      });
+    });
   };
 }
 
 // Synchronous Action
-export function decrement() {
+export function increment() {
   return {
-    type: types.COUNTER_DECREMENT,
+    type: types.COUNTER_INCREMENT,
   };
 }
